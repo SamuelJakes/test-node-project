eb042b227d1ac2116c51e24b7317d3105914a321
sjak6118 <sjak6118@uni.sydney.edu.au>
2020-11-01 13:35:48 +1100

    nodejs files added

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require("http")
+
+const hostname = "127.0.0.1"
+const port = 3000
+
+// req = request, has headers and request data
+// res = response, has server's response details that need setting
+const server = http.createServer((req, res) => {
+    // 200 = successful response
+    res.statusCode = 200
+
+    //setting the content of the response
+    res.setHeader('Content-Type', 'text/plain')
+
+    // when we close the response we send the contents with it as an argument
+    res.end("Hello World!\n")
+})
+
+// Tell the server to start and give it the ip and port to run on
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`)
+})
